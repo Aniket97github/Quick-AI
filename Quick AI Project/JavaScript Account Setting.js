649aeb232667e24761523446b590ad0861a00ee4
pc <pc@DESKTOP-HHOK8FO>
2023-05-06 19:26:14 +0530

    06/05/2023

--- Quick AI Project/JavaScript Account Setting.js
+++ Quick AI Project/JavaScript Account Setting.js
@@ -1,5 +1,19 @@
-﻿$('#btnToggle').on('click', function () {
-    //$("#sidebar").toggleClass('col-lg-3 full-width');
-    $("#sidebar").toggle();//left side bar id
-    $("#content").toggleClass('col-lg-12 full-width');//right side bar
-});
+﻿////$('#btnToggle').on('click', function () {
+////    //$("#sidebar").toggleClass('col-lg-3 full-width');
+////    $("#sidebar").toggle();//left side bar id
+////    $("#content").toggleClass('col-lg-12 full-width');//right side bar
+////});
+    $(document).ready(function () {
+        $("#DropDownList2").change(function () {
+          
+            if (this.value == 'Personal')
+            {
+                $("#TextBox10").hide();
+            }
+            else if (this.value == 'Business')
+            {
+                $("#TextBox10").show();
+            }
+        }); 
+      }); 
+    
